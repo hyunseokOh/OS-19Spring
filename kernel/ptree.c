9d64084d6ded804250f74156bdd8232aa63aa8fe
ktaebum <phya.ktaebum@gmail.com>
2019-03-15 15:17:47 +0900

    kernel buf allocation

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -11,9 +11,11 @@
  *
  */
 
+#include <linux/list.h>       /* linked likst usage */
 #include <linux/prinfo.h>     /* prinfo struct */
 #include <linux/sched.h>      /* task struct */
 #include <linux/sched/task.h> /* task_list lock */
+#include <linux/slab.h>       /* kmalloc, kfree */
 #include <linux/syscalls.h>   /* SYSCALL_DEFINE */
 #include <linux/uaccess.h>    /* user-space access */
 
@@ -21,6 +23,8 @@
 
 SYSCALL_DEFINE2(ptree, struct prinfo*, buf, int*, nr) {
   int nrValue; /* nr length */
+  int64_t result;
+  struct prinfo* kernelBuf = NULL;
 
   /* EINVAL check */
   if (buf == NULL || nr == NULL) {
@@ -37,5 +41,25 @@ SYSCALL_DEFINE2(ptree, struct prinfo*, buf, int*, nr) {
     return -EINVAL;
   }
 
+  kernelBuf = (struct prinfo*)kmalloc(sizeof(struct prinfo) * nrValue, GFP_KERNEL);
+  if (kernelBuf == NULL) {
+    /* allocation failed */
+    return -ENOMEM;
+  }
+
+  /* task lock */
+  read_lock(&tasklist_lock);
+
+  /* actual traversal goes here */
+
+  /* unlock */
+  read_unlock(&tasklist_lock);
+
+  if (copy_to_user(buf, kernelBuf, sizeof(struct prinfo) * nrValue)) {
+    /* copying kernel to user failed */
+    return -EFAULT;
+  }
+
+  kfree(kernelBuf);
   return 0;
 }
