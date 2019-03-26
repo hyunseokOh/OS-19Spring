674f635289b725dadcacffcf31778084b5d5226c
ktaebum <phya.ktaebum@gmail.com>
2019-03-27 00:57:52 +0900

    include types.h in rotation.h, implemented each syscall skeleton

--- include/linux/rotation.h
+++ include/linux/rotation.h
@@ -1,6 +1,8 @@
 #ifndef _LINUX_ROTATION_H
 #define _LINUX_ROTATION_H
 
+#include <linux/types.h>
+
 int64_t set_rotation(int degree);
 int64_t rotlock_read(int degree, int range);
 int64_t rotlock_write(int degree, int range);
