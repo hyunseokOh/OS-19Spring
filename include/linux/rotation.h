02a90ce6ef2aac7e65181e8d1d490e966a4b93f3
Hyunseok <hyunseok0222@gmail.com>
2019-03-27 00:36:56 +0900

    Change return types, convention styles

--- include/linux/rotation.h
+++ include/linux/rotation.h
@@ -1,10 +1,10 @@
-#ifndef _ROTATION_H
-#define _ROTATION_H
+#ifndef _LINUX_ROTATION_H
+#define _LINUX_ROTATION_H
 
-int set_rotation(int degree);
-int rotlock_read(int degree, int range);
-int rotlock_write(int degree, int range);
-int rotunlock_read(int degree, int range);
-int rotunlock_write(int degree, int range);
+int64_t set_rotation(int degree);
+int64_t rotlock_read(int degree, int range);
+int64_t rotlock_write(int degree, int range);
+int64_t rotunlock_read(int degree, int range);
+int64_t rotunlock_write(int degree, int range);
 
 #endif
