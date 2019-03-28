944d7b77674d2ebbf96fe6f2302738fe4d4b01e3
ktaebum <phya.ktaebum@gmail.com>
2019-03-28 15:58:29 +0900

    remove copy_from_user (user does not pass pointer to kernel)

--- kernel/rotation.c
+++ kernel/rotation.c
@@ -19,75 +19,22 @@ int64_t rotunlock_read(int degree, int range) { return 0; }
 int64_t rotunlock_write(int degree, int range) { return 0; }
 
 asmlinkage long sys_set_rotation(int degree) {
-  int kernelDegree;
-  if (copy_from_user(&kernelDegree, &degree, sizeof(int))) {
-    /* EFAULT occur (cannot access) */
-    return -EFAULT;
-  }
   set_rotation(kernelDegree);
   return 0;
 }
 
 asmlinkage long sys_rotlock_read(int degree, int range) {
-  int kernelDegree;
-  int kernelRange;
-
-  if (copy_from_user(&kernelDegree, &degree, sizeof(int))) {
-    /* EFAULT occur (cannot access) */
-    return -EFAULT;
-  }
-
-  if (copy_from_user(&kernelRange, &range, sizeof(int))) {
-    /* EFAULT occur (cannot access) */
-    return -EFAULT;
-  }
   return 0;
 }
 
 asmlinkage long sys_rotlock_write(int degree, int range) {
-  int kernelDegree;
-  int kernelRange;
-
-  if (copy_from_user(&kernelDegree, &degree, sizeof(int))) {
-    /* EFAULT occur (cannot access) */
-    return -EFAULT;
-  }
-
-  if (copy_from_user(&kernelRange, &range, sizeof(int))) {
-    /* EFAULT occur (cannot access) */
-    return -EFAULT;
-  }
   return 0;
 }
 
 asmlinkage long sys_rotunlock_read(int degree, int range) {
-  int kernelDegree;
-  int kernelRange;
-
-  if (copy_from_user(&kernelDegree, &degree, sizeof(int))) {
-    /* EFAULT occur (cannot access) */
-    return -EFAULT;
-  }
-
-  if (copy_from_user(&kernelRange, &range, sizeof(int))) {
-    /* EFAULT occur (cannot access) */
-    return -EFAULT;
-  }
   return 0;
 }
 
 asmlinkage long sys_rotunlock_write(int degree, int range) {
-  int kernelDegree;
-  int kernelRange;
-
-  if (copy_from_user(&kernelDegree, &degree, sizeof(int))) {
-    /* EFAULT occur (cannot access) */
-    return -EFAULT;
-  }
-
-  if (copy_from_user(&kernelRange, &range, sizeof(int))) {
-    /* EFAULT occur (cannot access) */
-    return -EFAULT;
-  }
   return 0;
 }
