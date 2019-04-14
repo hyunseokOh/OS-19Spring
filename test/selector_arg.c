ee0fd1400d8d535f40104f931809e9abda39730b
Seong Hoon Seo <andyseo247@gmail.com>
2019-04-14 19:13:13 +0900

    add error-handling for additional testing files

--- test/selector_arg.c
+++ test/selector_arg.c
@@ -20,6 +20,11 @@ void intHandler(int sig) {
 int main(int argc, char *argv[]) {
   int num;
   long longNum;
+  int degree;
+  long longDegree;
+  int range;
+  long longRange;
+
   FILE *fp = NULL;
   char *endPtr = NULL;
 
@@ -29,10 +34,6 @@ int main(int argc, char *argv[]) {
   }
 
   longNum = strtol(argv[1], &endPtr, 10);
-  /* TODO:(seonghoon) change depreciated atoi() to strtol with error-handling */
-  int degree = atoi(argv[2]);
-  int range = atoi(argv[3]);
-
 
   if (*endPtr) {
     /* endPtr points to NULL char only when the entire argument is a number */
@@ -49,10 +50,43 @@ int main(int argc, char *argv[]) {
     num = (int)longNum;
   }
 
+  longDegree = strtol(argv[2], &endPtr, 10);
+
+  if (*endPtr) {
+    /* endPtr points to NULL char only when the entire argument is a number */
+    printf("INVALID ARGUMENT: invalid character exists or no digits at all\n");
+    return 0;
+  } else if (errno != 0) {
+    /* errno is set to ERANGE if underflow or overflow occurs */
+    printf("INVALID ARGUMENT: most likely overflow or underflow\n");
+    return 0;
+  } else if ((longDegree > INT_MAX) || (longDegree < INT_MIN)) {
+    printf("INVALID ARGUMENT: number cannot be expressed as int'\n");
+    return 0;
+  } else {
+    degree = (int)longDegree;
+  }
+
+  longRange = strtol(argv[3], &endPtr, 10);
+
+  if (*endPtr) {
+    /* endPtr points to NULL char only when the entire argument is a number */
+    printf("INVALID ARGUMENT: invalid character exists or no digits at all\n");
+    return 0;
+  } else if (errno != 0) {
+    /* errno is set to ERANGE if underflow or overflow occurs */
+    printf("INVALID ARGUMENT: most likely overflow or underflow\n");
+    return 0;
+  } else if ((longRange > INT_MAX) || (longRange < INT_MIN)) {
+    printf("INVALID ARGUMENT: number cannot be expressed as int'\n");
+    return 0;
+  } else {
+    range = (int)longRange;
+  }
+
   signal(SIGINT, intHandler);
 
   while (selectorRun) {
-//    syscall(SYSCALL_ROTLOCK_WRITE, 90, 90);
     syscall(SYSCALL_ROTLOCK_WRITE, degree, range);
     fp = fopen("integer", "w");
     if (fp != NULL) {
@@ -61,7 +95,6 @@ int main(int argc, char *argv[]) {
       fclose(fp);
       fp = NULL;
     }
-//    syscall(SYSCALL_ROTUNLOCK_WRITE, 90, 90);
     syscall(SYSCALL_ROTUNLOCK_WRITE, degree, range);
   }
 
--- test/trial_arg.c
+++ test/trial_arg.c
@@ -57,6 +57,11 @@ int main(int argc, char *argv[]) {
   int target;
   int id;
   long long_id;
+  int degree;
+  long longDegree;
+  int range;
+  long longRange;
+
   FILE *fp = NULL;
   char *endPtr = NULL;
 
@@ -66,9 +71,6 @@ int main(int argc, char *argv[]) {
   }
 
   long_id = strtol(argv[1], &endPtr, 10);
-  /* TODO:(seonghoon) change depreciated atoi() to strtol with error-handling */
-  int degree = atoi(argv[2]);
-  int range = atoi(argv[3]);
 
   if (*endPtr) {
     /* endPtr points to NULL char only when the entire argument is a number */
@@ -85,10 +87,43 @@ int main(int argc, char *argv[]) {
     id = (int)long_id;
   }
 
+  longDegree = strtol(argv[2], &endPtr, 10);
+
+  if (*endPtr) {
+    /* endPtr points to NULL char only when the entire argument is a number */
+    printf("INVALID ARGUMENT: invalid character exists or no digits at all\n");
+    return 0;
+  } else if (errno != 0) {
+    /* errno is set to ERANGE if underflow or overflow occurs */
+    printf("INVALID ARGUMENT: most likely overflow or underflow\n");
+    return 0;
+  } else if ((longDegree > INT_MAX) || (longDegree < INT_MIN)) {
+    printf("INVALID ARGUMENT: number cannot be expressed as int'\n");
+    return 0;
+  } else {
+    degree = (int)longDegree;
+  }
+
+  longRange = strtol(argv[3], &endPtr, 10);
+
+  if (*endPtr) {
+    /* endPtr points to NULL char only when the entire argument is a number */
+    printf("INVALID ARGUMENT: invalid character exists or no digits at all\n");
+    return 0;
+  } else if (errno != 0) {
+    /* errno is set to ERANGE if underflow or overflow occurs */
+    printf("INVALID ARGUMENT: most likely overflow or underflow\n");
+    return 0;
+  } else if ((longRange > INT_MAX) || (longRange < INT_MIN)) {
+    printf("INVALID ARGUMENT: number cannot be expressed as int'\n");
+    return 0;
+  } else {
+    range = (int)longRange;
+  }
+
   signal(SIGINT, intHandler);
 
   while (trialRun) {
-//    syscall(SYSCALL_ROTLOCK_READ, 90, 90);
     syscall(SYSCALL_ROTLOCK_READ, degree, range);
     fp = fopen("integer", "r");
     if (fp != NULL) {
@@ -97,7 +132,6 @@ int main(int argc, char *argv[]) {
       fclose(fp);
       fp = NULL;
     }
-//    syscall(SYSCALL_ROTUNLOCK_READ, 90, 90);
     syscall(SYSCALL_ROTUNLOCK_READ, degree, range);
   }
 
