52298e1ab56c58b9e09cb61c83f1bd7d04c0ef72
ktaebum <phya.ktaebum@gmail.com>
2019-04-30 14:08:55 +0900

    skel of wrr_entity in task_struct, and syscall skel

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -940,5 +940,7 @@ asmlinkage long sys_pkey_alloc(unsigned long flags, unsigned long init_val);
 asmlinkage long sys_pkey_free(int pkey);
 asmlinkage long sys_statx(int dfd, const char __user *path, unsigned flags,
 			  unsigned mask, struct statx __user *buffer);
+asmlinkage long sys_sched_set_weight(pid_t pid, int weight);
+asmlinkage long sys_sched_get_weight(pid_t pid);
 
 #endif
