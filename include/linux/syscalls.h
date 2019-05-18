1430c017d73986d38e0422e2d9a052b56866115c
ktaebum <phya.ktaebum@gmail.com>
2019-05-18 22:02:13 +0900

    proj4 start

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -79,6 +79,7 @@ union bpf_attr;
 #include <linux/unistd.h>
 #include <linux/quota.h>
 #include <linux/key.h>
+#include <linux/gps.h>
 #include <trace/syscall.h>
 
 /*
@@ -940,5 +941,6 @@ asmlinkage long sys_pkey_alloc(unsigned long flags, unsigned long init_val);
 asmlinkage long sys_pkey_free(int pkey);
 asmlinkage long sys_statx(int dfd, const char __user *path, unsigned flags,
 			  unsigned mask, struct statx __user *buffer);
+asmlinkage long sys_set_gps_location(struct gps_location __user *loc);
 
 #endif
