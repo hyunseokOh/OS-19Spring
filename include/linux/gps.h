06263ff572f9335013dc35693521c362c98066a1
ktaebum <phya.ktaebum@gmail.com>
2019-05-20 17:33:40 +0900

    simple error check in set_gps_location syscall

--- include/linux/gps.h
+++ include/linux/gps.h
@@ -9,4 +9,18 @@ struct gps_location {
   int accuracy;
 };
 
+static inline int valid_longitude(int longitude) {
+  return -180 <= longitude && longitude <= 180;
+}
+
+static inline int valid_latitude(int latitude) {
+  return -90 <= latitude && latitude <= 90;
+}
+
+static inline int valid_fractional(int fractional) {
+  return 0 <= fractional && fractional <= 999999;
+}
+
+int valid_gps_location(struct gps_location *loc);
+
 #endif
--- kernel/gps.c
+++ kernel/gps.c
@@ -1,6 +1,38 @@
 #include <linux/syscalls.h>
 #include <linux/gps.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+
+#include <uapi/asm-generic/errno-base.h>
+
+int valid_gps_location(struct gps_location *loc) {
+  return valid_longitude(loc->lng_integer) &&
+    valid_latitude(loc->lat_integer) &&
+    valid_fractional(loc->lat_fractional) &&
+    valid_fractional(loc->lng_fractional);
+}
 
 SYSCALL_DEFINE1(set_gps_location, struct gps_location __user *, loc) {
-  return 0;
+  struct gps_location *kloc;
+  int retval = 0;
+
+  kloc = (struct gps_location *)kmalloc(sizeof(struct gps_location), GFP_KERNEL);
+
+  if (kloc == NULL) {
+    return -ENOMEM;
+  }
+
+  if (copy_from_user(kloc, loc, sizeof(struct gps_location))) {
+    retval = -EFAULT;
+    goto free_and_return;
+  }
+
+  if (!valid_gps_location(kloc)) {
+    retval = -EINVAL;
+    goto free_and_return;
+  }
+
+free_and_return:
+  kfree(kloc);
+  return retval;
 }
